e7dd4760acc233063908cfc0b7cabc2da81078fd
Braden Walker <bradencodes@gmail.com>
2019-07-08 16:44:27 -0700

    fix edit profile bug

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -35,15 +35,30 @@ const EditProfile = ({
       website: loading || !profile.website ? '' : profile.website,
       location: loading || !profile.location ? '' : profile.location,
       status: loading || !profile.status ? '' : profile.status,
-      skills: loading || !profile.skills ? '' : profile.skills,
+      skills: loading || !profile.skills ? '' : profile.skills.join(', '),
       githubusername:
         loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.twitter ? '' : profile.twitter,
-      facebook: loading || !profile.facebook ? '' : profile.facebook,
-      linkedin: loading || !profile.linkedin ? '' : profile.linkedin,
-      youtube: loading || !profile.youtube ? '' : profile.youtube,
-      instagram: loading || !profile.instagram ? '' : profile.instagram
+      twitter:
+        loading || !profile.social || !profile.social.twitter
+          ? ''
+          : profile.social.twitter,
+      facebook:
+        loading || !profile.social || !profile.social.facebook
+          ? ''
+          : profile.social.facebook,
+      linkedin:
+        loading || !profile.social || !profile.social.linkedin
+          ? ''
+          : profile.social.linkedin,
+      youtube:
+        loading || !profile.social || !profile.social.youtube
+          ? ''
+          : profile.social.youtube,
+      instagram:
+        loading || !profile.social || !profile.social.instagram
+          ? ''
+          : profile.social.instagram
     });
   }, [loading, getCurrentProfile]);
 
